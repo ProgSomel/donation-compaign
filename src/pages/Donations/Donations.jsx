f710cd2b5b71f846fc6d408a41b214305ec2f930
Somel <somelahmed88@gmail.com>
2023-09-26 01:18:24 +0600

    see all button for donatioin page Done

--- src/pages/Donations/Donations.jsx
+++ src/pages/Donations/Donations.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Donation from "../Donation/Donation";
+import Swal from "sweetalert2";
 
 const Donations = () => {
 
     const donations = JSON.parse( localStorage.getItem('donation'))
 
+    const [isSeeAll, setSeeAll] = useState(false);
+
+    useEffect(() => {
+        if (!donations) {
+          Swal.fire('You have not Donated yet');
+        }
+      }, [donations]);
     
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 my-8 md:my-16">
+        <div>
+                   <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 md:mt-16 mb-7">
             {
-                donations.map(donation => <Donation key={donation.id} donation={donation}></Donation>)
+                donations ?  isSeeAll ? (donations.map(donation => <Donation key={donation.id} donation={donation}></Donation>)) : (donations?.slice(0,4).map(donation => <Donation key={donation.id} donation={donation}></Donation>)): ""
+              
             }
+        </div>
+        <div className="text-center mb-8">
+            {
+               donations ? donations.length > 4 &&(isSeeAll ? <button className="bg-red-300 px-2 py-1 rounded-lg" onClick={()=> setSeeAll(!isSeeAll)}>See less</button>: <button className="bg-green-300 px-2 py-1 rounded-lg " onClick={()=>setSeeAll(!isSeeAll)}>See All</button> ) :""
+            }
+            </div>
+            
         </div>
     );
 };
